edc8e0e9b5d89ecbec55bf5f420a28128c5c8517
Matt Bailey <baileyma@tcd.ie>
2024-10-16 17:37:18 +0100

    Complete JS code for index.html form functionality

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,52 +2,78 @@ const commentsList = [
   {
     name: 'Victor Pinto',
 
-    Comment:
+    comment:
       'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
 
-    Date: '11/02/2023',
+    date: '11/02/2023',
   },
 
   {
     name: 'Christina Cabrera',
 
-    Comment:
+    comment:
       'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
 
-    Date: '10/28/2023',
+    date: '10/28/2023',
   },
 
   {
     name: 'Isaac Tadesse',
 
-    Comment:
+    comment:
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
 
-    Date: '10/20/2023',
+    date: '10/20/2023',
   },
 ];
 
-let commentsSectionElA = document.querySelector('.comments__all-text');
-let commentsSectionElB = document.querySelector('.comments__title-and-date');
+let commentsSectionEl = document.querySelector('.comments__title-and-date');
 
+// Make the comments form div a variable
+
+// fn that takes event object's name, title, and comment
+// and puts it at the top of the commentsList
+
+// This puts commentsList into p tags within div
+// creates p element, gives innerhtml from commentsList, adds class, then appends to div
 function displayComment() {
+  commentsSectionEl.innerHTML = '';
+
   for (let i = 0; i < commentsList.length; i++) {
     let commentNameEl = document.createElement('p');
     let commentDateEl = document.createElement('p');
     let commentTextEl = document.createElement('p');
 
     commentNameEl.innerHTML = commentsList[i].name;
-    commentDateEl.innerHTML = commentsList[i].Date;
-    commentTextEl.innerHTML = commentsList[i].Comment;
+    commentDateEl.innerHTML = commentsList[i].date;
+    commentTextEl.innerHTML = commentsList[i].comment;
 
     commentNameEl.classList.add('comments__name');
     commentDateEl.classList.add('comments__date');
     commentTextEl.classList.add('comments__text');
 
-    commentsSectionElA.appendChild(commentNameEl);
-    commentsSectionElA.appendChild(commentDateEl);
-    commentsSectionElB.appendChild(commentTextEl);
+    commentsSectionEl.appendChild(commentNameEl);
+    commentsSectionEl.appendChild(commentDateEl);
+    commentsSectionEl.appendChild(commentTextEl);
   }
 }
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  let newComment = {
+    name: event.target.name.value,
+    date: event.timeStamp,
+    comment: event.target.comment.value,
+  };
+
+  commentsList.unshift(newComment);
+
+  displayComment();
+};
+
+// This grabs the form div, and then chucks its info into the top of the commentsList
+
+let form = document.querySelector('.comments__form');
+form.addEventListener('submit', (element) => submitHandler(element));
 
 displayComment();
