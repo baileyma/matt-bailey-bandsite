a47df0057d3ab4a18b2c19a60e5ceaa9f36fa6a3
Matt Bailey <baileyma@tcd.ie>
2024-10-18 21:02:14 +0100

    Sort optional titles in shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -52,6 +52,8 @@ function displayShows() {
   showsListEl.innerHTML = '';
 
   for (let i = 0; i < showsList.length; i++) {
+    const itemWrapperEl = document.createElement('div');
+
     const dateTitleEl = document.createElement('h3');
     dateTitleEl.innerHTML = 'DATE';
 
@@ -70,6 +72,8 @@ function displayShows() {
     const locationEl = document.createElement('p');
     locationEl.innerHTML = showsList[i].location;
 
+    itemWrapperEl.classList.add('shows__item-wrapper');
+
     dateTitleEl.classList.add('shows__title');
     dateEl.classList.add('shows__date');
     venueTitleEl.classList.add('shows__title');
@@ -81,13 +85,14 @@ function displayShows() {
     buttonEl.innerHTML = "<button class='shows__button'>BUY TICKETS</button>";
     buttonEl.classList.add('shows__button-container');
 
-    showsListEl.appendChild(dateTitleEl);
-    showsListEl.appendChild(dateEl);
-    showsListEl.appendChild(venueTitleEl);
-    showsListEl.appendChild(venueEl);
-    showsListEl.appendChild(locationTitleEl);
-    showsListEl.appendChild(locationEl);
-    showsListEl.appendChild(buttonEl);
+    itemWrapperEl.appendChild(dateTitleEl);
+    itemWrapperEl.appendChild(dateEl);
+    itemWrapperEl.appendChild(venueTitleEl);
+    itemWrapperEl.appendChild(venueEl);
+    itemWrapperEl.appendChild(locationTitleEl);
+    itemWrapperEl.appendChild(locationEl);
+    itemWrapperEl.appendChild(buttonEl);
+    showsListEl.appendChild(itemWrapperEl);
   }
 }
 
